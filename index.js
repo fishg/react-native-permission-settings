35d6017d69a2a7598c608cad19779e299745183e
fryossi <fryossi@users.noreply.github.com>
2018-02-28 21:36:43 +0200

    change the return to be Promise

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
 'use strict';
 import { NativeModules } from 'react-native';
-module.exports = NativeModules.PermissionSettings;
+const { PermissionSettings } = NativeModules;
+
+class AndroidNotificationsPermission {
+  
+  areNotificationsEnabled() {
+    new Promise((resolve, reject) => {
+      PermissionSettings.areNotificationsEnabled((isEnable, error) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(isEnable);
+        }
+      });
+    }),
+  }
+}
+
+module.exports = new AndroidNotificationsPermission();
