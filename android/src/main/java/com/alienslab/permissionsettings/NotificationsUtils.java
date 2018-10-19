8819bf13498f0329f8cce34d68ce52ccdf470e4e
Sean Timm <sean@seantimm.com>
2018-10-19 10:52:28 -0600

    Fix Android notification detection
    The old reflection-based method no longer works for newer Android
    APIs.  Switching to use NotificationManagerCompat which should
    work for API 19+.

--- android/src/main/java/com/alienslab/permissionsettings/NotificationsUtils.java
+++ android/src/main/java/com/alienslab/permissionsettings/NotificationsUtils.java
@@ -1,47 +1,14 @@
 package com.alienslab.permissionsettings;
 
-import android.app.AppOpsManager;
 import android.content.Context;
-import android.content.pm.ApplicationInfo;
-
-import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
+import android.support.v4.app.NotificationManagerCompat;
 
 /**
  * Created by applepc on 26/09/2017.
  */
 
 public class NotificationsUtils {
-
-    private static final String CHECK_OP_NO_THROW = "checkOpNoThrow";
-    private static final String OP_POST_NOTIFICATION = "OP_POST_NOTIFICATION";
-
     public static boolean isNotificationEnabled(Context context) {
-
-        try {
-            AppOpsManager mAppOps = (AppOpsManager) context.getSystemService(Context.APP_OPS_SERVICE);
-
-            ApplicationInfo appInfo = context.getApplicationInfo();
-
-            String pkg = context.getApplicationContext().getPackageName();
-
-            int uid = appInfo.uid;
-
-            Class appOpsClass = null; /* Context.APP_OPS_MANAGER */
-
-            appOpsClass = Class.forName(AppOpsManager.class.getName());
-
-            Method checkOpNoThrowMethod = appOpsClass.getMethod(CHECK_OP_NO_THROW, Integer.TYPE, Integer.TYPE, String.class);
-
-            Field opPostNotificationValue = appOpsClass.getDeclaredField(OP_POST_NOTIFICATION);
-            int value = (int) opPostNotificationValue.get(Integer.class);
-
-            return ((int) checkOpNoThrowMethod.invoke(mAppOps, value, uid, pkg) == AppOpsManager.MODE_ALLOWED);
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return false;
+        return NotificationManagerCompat.from(context).areNotificationsEnabled();
     }
 }
